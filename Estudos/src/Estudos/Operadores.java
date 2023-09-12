2b8851952f96d4b4e93182e5fc67ad97fb439dec
HalisonRoque <halisonyasha@gmail.com>
2023-09-12 18:16:26 -0300

    operadores

---
+++ Estudos/src/Estudos/Operadores.java
@@ -0,0 +1,19 @@
+package Estudos;
+
+public class Operadores {
+    public static void main(String[] args){
+        int numero = 5;
+// numero positivo com o sinal (-) deixa o numero negativo, se não tiver outras operações relacionadas
+        numero = - numero;
+// print o valor de numero.
+        System.out.println(numero);
+// o numero permanece negativo pois a variavel numero ja vem negativa, e o ssinal de (+) nesse caso não trasnforma em um numero positivo
+        numero = + numero;
+        System.out.println(numero);
+// para transformar em positivo devemos fazer uma multiplicação simples.
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+    }
+}
