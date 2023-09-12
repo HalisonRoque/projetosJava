b9ecbf345a2efd028774e23ad4c5a79ec47e4545
HalisonRoque <halisonyasha@gmail.com>
2023-09-11 22:08:01 -0300

    Create Calculadora.java
    calculadora

---
+++ Estudos/src/Estudos/Calculadora.java
@@ -0,0 +1,17 @@
+package Estudos;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("digite um valor= ");
+        int x = scan.nextInt();
+
+        for(int i = 0; i <= 10; i++){
+            int resul = x * i;
+            System.out.println(x +" x " + i + " = " + resul);
+        }
+    }
+}
