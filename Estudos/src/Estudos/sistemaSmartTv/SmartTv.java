0836deac0a807217a1fb1f5abcd765b9fc1f495c
HalisonRoque <halisonyasha@gmail.com>
2023-09-13 20:46:26 -0300

    adicionando os metodos
    adicionado os metodos para a SmartTv

--- Estudos/src/Estudos/sistemaSmartTv/SmartTv.java
+++ Estudos/src/Estudos/sistemaSmartTv/SmartTv.java
@@ -4,4 +4,39 @@ public class SmartTv {
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentar o Volume Para = " + volume);
+    }
+
+    public void diminiurVolume(){
+        volume--;
+        System.out.println("Diminuir o Volume Para = " + volume);
+    }
+    public void aumentarCanal(){
+        canal++;
+        System.out.println("CANAL " + canal);
+    }
+    public void diminuirCanal(){
+        canal--;
+        System.out.println("CANAL " + canal);
+    }
+    public void mudarCanal(int novoCanal){
+        if(novoCanal <= 30){
+            this.canal = novoCanal;
+            System.out.println("CANAL " + canal);
+        }else{
+            System.out.println("CANAL NÃO ENCONTRADO.");
+        }
+
+    }
 }
--- Estudos/src/Estudos/sistemaSmartTv/Usuario.java
+++ Estudos/src/Estudos/sistemaSmartTv/Usuario.java
@@ -1,13 +1,23 @@
 package Estudos.sistemaSmartTv;
 
+import java.util.Scanner;
+
 public class Usuario {
     public static void main(String[] args){
         SmartTv smartTv = new SmartTv();
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("TV ligada = " + smartTv.ligada);
-        System.out.println("Canal Atual = " + smartTv.canal);
-        System.out.println("Volume Atual = " + smartTv.volume);
 
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
 
+        smartTv.mudarCanal(31);
+        smartTv.aumentarVolume();
+        smartTv.diminuirCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        System.out.println("canal atual " + smartTv.canal);
     }
 }
