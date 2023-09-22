206fd6b1f611437d7fe16db4f4a98298814c242a
HalisonRoque <halisonyasha@gmail.com>
2023-09-21 21:47:18 -0300

    documentando a classe

--- Estudos/src/Estudos/aprendizado/ArgumentosMain.java
+++ Estudos/src/Estudos/aprendizado/ArgumentosMain.java
@@ -2,6 +2,11 @@ package Estudos.aprendizado;
 
 public class ArgumentosMain {
     public static  void main (String[] args){
+        /**
+        funções para executar no terminal pelo prompt de comando
+         acessando os argumentos do args[] em um array.
+         imprimindo seus parametros dentro do array.
+        */
         String nome = args[0];
         String sobrenome = args[1];
         int idade = Integer.parseInt(args[2]);
