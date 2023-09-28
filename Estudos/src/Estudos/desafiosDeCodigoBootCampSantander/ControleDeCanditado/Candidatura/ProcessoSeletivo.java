568e30d20b9adb382e15a94fa53953164aad6cb9
HalisonRoque <halisonyasha@gmail.com>
2023-09-27 23:13:29 -0300

    processo seletivo

---
+++ Estudos/src/Estudos/desafiosDeCodigoBootCampSantander/ControleDeCanditado/Candidatura/ProcessoSeletivo.java
@@ -0,0 +1,44 @@
+package Estudos.desafiosDeCodigoBootCampSantander.ControleDeCanditado.Candidatura;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args){
+       selecaoCandidatos();
+    }
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","MIRELA","DANIELA","JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+           String canditato = candidatos[candidatoAtual];
+           double salarioPretendido = valorPretendido();
+
+           System.out.println("O candidato " + canditato + "Solicitou este valor de salário " + salarioPretendido);
+           if (salarioBase >= salarioPretendido) {
+               System.out.println("O candidato " + canditato + " foi selecionado para a vaga");
+               candidatosSelecionados++;
+           }
+           candidatoAtual++;
+        }
+
+    }
+
+    private static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);;
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        } else if (salarioBase==salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+
+        }else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+}
