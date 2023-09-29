3a4fbe0089cf8cfe2f7eea70ea10ffb8e7085071
HalisonRoque <halisonyasha@gmail.com>
2023-09-28 21:23:34 -0300

    LIGANDO PARA CANDIDATOS

--- Estudos/src/Estudos/desafiosDeCodigoBootCampSantander/ControleDeCanditado/Candidatura/ProcessoSeletivo.java
+++ Estudos/src/Estudos/desafiosDeCodigoBootCampSantander/ControleDeCanditado/Candidatura/ProcessoSeletivo.java
@@ -1,11 +1,41 @@
 package Estudos.desafiosDeCodigoBootCampSantander.ControleDeCanditado.Candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args){
-       imprimirSelecionados();
+        String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO"};
+        for (String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
     }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando){
+                tentativasRealizadas++;
+            } else {
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+            }
+        }while (continuarTentando && tentativasRealizadas < 3);
+        if (atendeu){
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+        } else{
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO DE TENTATIVAS " + tentativasRealizadas + " REALIZADAS");
+        }
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
+    }
+
+
     static void imprimirSelecionados(){
         String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO"};
         System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
